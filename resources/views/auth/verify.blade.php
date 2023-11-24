eb38d513bce3459c2fec789fdddb499071229fbe
Tharaka Harischandra <tharakaez@gmail.com>
2023-11-24 06:58:20 +0530

    Login, Register & Email Verify Done

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,42 @@
-@extends('layouts.app')
-
+@extends('layouts.layout1')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<!--  Body Wrapper -->
+<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <div
+        class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+        <div class="d-flex align-items-center justify-content-center w-100">
+            <div class="row justify-content-center w-100">
+                <div class="col-md-8 col-lg-6 col-xxl-3">
+                    <div class="card mb-0">
+                        <div class="card-body">
+                            <a href="#" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                                <img src="{{ asset('assets/images/logos/title logo.png') }}" width="180"
+                                    alt="">
+                            </a>
+                            <p class="text-center">Verify Your Email Address</p>
+                            @if (session('resent'))
+                                <div class="alert alert-info py-3 " role="alert">
+                                    {{ __('A fresh verification link has been sent to your email address.') }}
+                                </div>
+                            @endif
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            <form class="" method="POST" action="{{ route('verification.resend') }}">
+                                @csrf
+                                <div class="mb-4">
+                                    <h6 class=" text-center ">Before proceeding, please check your email for a verification link.</h6>
+                                    <h6 class=" text-center ">If you did not receive the email,</h6>
+                                </div>
+                                    <button type="submit"
+                                class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Re-Send</button>.
+                                
+                            </form>
                         </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<!--  Body Wrapper -->
 @endsection
--- resources/views/libraries/userScript.blade.php
+++ resources/views/libraries/userScript.blade.php
@@ -7,15 +7,10 @@
 <script src="{{ asset('assets/js/dashboard.js') }}"></script>
 <script src="{{ asset('assets/js/formValidate.js') }}"></script>
 
-{{-- <script src="{{ asset('assets/libs/toastr/build/toastr.min.js') }}"></script>
-<script src="{{ asset('assets/libs/toastr/toastr-init.js') }}"></script>
-<script src="{{ asset('assets/libs/sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
-<script src="{{ asset('assets/libs/sweetalert2/sweet-alert.init.js') }}"></script>
-<script src="{{ asset('assets/libs/select2/dist/js/select2.full.min.js') }}"></script>
-<script src="{{ asset('assets/libs/select2/dist/js/select2.min.js') }}"></script>
-<script src="{{ asset('assets/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script> --}}
 
 {{-- Bootstrap JS --}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
+{{-- JQuary --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
